b3a16f5322d7510f559b6b9b2a515164aa85feef
Jay <1421128167@qq.com>
2018-04-11 03:21:33 +0800

    Update OSSUtil.java

--- src/main/java/com/mmall/util/OSSUtil.java
+++ src/main/java/com/mmall/util/OSSUtil.java
@@ -16,13 +16,13 @@ public class OSSUtil {
 
     public static final String ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
     //阿里云api的密钥Access Key ID
-    public static final String ACCESS_KEY_ID = "LTAIYnp0eNL9HOxW";
+    public static final String ACCESS_KEY_ID = "";
     //阿里云api的密钥Access Key Secret
-    public static final String Access_KEY_SECRET = "p8KioGqjgIpoGRVh6ySfq8tkTivdjK";
+    public static final String Access_KEY_SECRET = "";
     //阿里云api的bucket名称
-    public static final String BUCKET_NAME = "uploadsomefiles";
+    public static final String BUCKET_NAME = "";
     //阿里云api的文件夹名称
-    public static final String FOLDER = "jay/";
+    public static final String FOLDER = "";
 
 
     public ModelAndView addStudent(MultipartFile file) throws IOException {
